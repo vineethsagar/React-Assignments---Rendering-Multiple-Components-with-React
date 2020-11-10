a18b7be0df583d71fa20939becebb9fcd5601810
vineethsagar <30525698+vineethsagar@users.noreply.github.com>
2020-11-10 12:14:07 +0530

    added name and description

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
   return (
-    <></>
-  )
-}
-
+    <div>
+      <h1 data-ns-test="project-name">Balb</h1>
+      <h6 data-ns-test="project-description">Balb is a twitter clone</h6>
+    </div>
+  );
+};
 
 export default App;
